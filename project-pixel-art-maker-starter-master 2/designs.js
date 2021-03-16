1808e4d08c2801d8993a70c0988abcef7fb01b67
AL-escp <79520223+AL-escp@users.noreply.github.com>
2021-03-16 15:19:57 +0100

    Update designs.js

--- project-pixel-art-maker-starter-master 2/designs.js
+++ project-pixel-art-maker-starter-master 2/designs.js
@@ -1,56 +1,50 @@
-
 // Select color input --> done
-// Select size input--> done
+// Select size input --> done
 
-// When size is submitted by the user, call makeGrid() --> tested and works
+const getTable = () => document.getElementById("pixelCanvas");
+const getColorPicker = () => document.getElementById("colorPicker");
+const getTableCells = () => document.getElementsByClassName('cell');
+const getTableHeightInput = () => document.getElementById('inputHeight').value;
+const getTableWidthInput = () => document.getElementById('inputWidth').value;
 
-// gets values for height and width from form and uses them to call makrGrid()
+// When size is submitted by the user, call makeGrid()
+const makeGrid = (height, width) => { //makeGrid function definition of parameters
+  createGrid(height, width);
+  addClickEventToCells(); //const call
+}
 
-function formSubmission() {
+const createGrid = (height, width) => {
+  const table = getTable();
+  table.innerHTML=''; // innerHTML Property referring to the DOM
+  for (let i = 0; i < height; i++) { //loop for input
+    const row = table.insertRow();
+    for (let j = 0; j < width; j++) { //for loop defining the incert cell method
+      const cell = row.insertCell(); //insert cell method
+      cell.className = "cell"; //definition of the cell class
+    }
+  }
+}
+
+const onCellClick = event => { //the event function
+     const clickedCell = event.target;  // the event call is a users-click documented in the DOM
+     clickedCell.style.backgroundColor = colorPicker.value; //style backgroundcolor
+     clickedCell.innerHTML= ' :)'; // when the clickedCell event gets called, it calls the dom accordingly
+     console.log(clickedCell.innerHTML);
+ }
+
+const addClickEventToCells = () => { // add click events to all cells
+    const colorPicker = getColorPicker(); //allows user to choose color
+    const cells = getTableCells(); //retreives the cell from the cell class
+    Array.from(cells).forEach(cell => {
+      cell.addEventListener("click", onCellClick);
+    })
+}
+
+document.getElementById('sizePicker').onsubmit = () => { //this determines the selection of a user
     event.preventDefault();
-    const height = document.getElementById('inputHeight').value; //definition of the height
-    const width = document.getElementById('inputWidth').value;//definition of the width
-    makeGrid(height, width); //call the make grid
-}
+    const height = getTableHeightInput(); //height input
+    const width = getTableWidthInput(); //width input
+    makeGrid(height, width); //height and width selection
+}; //without this, the user input would be null
 
-// add click events to all cells
-function addClickEventToCells() {
-    //  color selection return --> above
-    const colorPicker = document.getElementById("colorPicker"); //allow people to pick the color for the boxes
-    const cells = document.getElementsByClassName('cell');
-    for (let i = 0; i < cells.length; i++) { //got loop of Listener
-        cells[i].addEventListener("click", function (event) { //listener addition, for noe command
-            let clickedCell = event.target;
-            clickedCell.style.backgroundColor = colorPicker.value;
-        });
-    }
-
-
-//makeGrid function
-function makeGrid(height, width) { //attributes requered
-    const table = document.getElementById("pixelCanvas");
-    let grid = '';
-
-    // loop over each row
-    for (let i = 0; i < height; i++) { //loop the actioms accordingly
-        grid += '<tr class="row-' + i + '">';
-        // loop for each cell - be careful with sequence of for look
-        for (let j = 0; j < width; j++) {
-            grid += '<td class="cell" id="row-' + i + '_cell-' + j + '"></td>';
-        }
-        grid += '</tr>'; // //row of cell
-    }
-    // grid into table element
-    table.innerHTML = grid;
-
-    // Add click event to grid cells once the table grid has been created. This is very important
-    addClickEventToCells();
-}
-
-// on submit of form #sizePicker --> this is what was above
-document.getElementById('sizePicker').onsubmit = function () {
-    formSubmission(); //submit button
-};
-
-//default grid, this varilbe can be change accordingly in the code. In this casw 5x5
-makeGrid(5, 5);
+makeGrid(3,3); //a pre-existing grid, that users can already interact with
